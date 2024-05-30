867101f99a56ce0f6d79ab60c3bc3fdbf246cf5a
Vanessa Araújo <lucenaaraujo@prof.educacao.sp.gov.br>
2024-05-30 02:42:59 -0300

    Construção main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+let nomeUsuário = "";
+let elemento = document.querySelector("#nome-usuário");
+
+while(nomeUsuário == ""){
+    nomeUsuário = prompt("Qual seu nome?");
+}
+
+if(nomeUsuário == null){
+elemento.textContent = 'Seja bem-vindo!';
+}else{
+    elemento.textContent = nomeUsuário
+}  
+
